9134d3bb8c9c1b3c28dcf423c1c8600b298e4b44
Brittne <brittnebaila@gmail.com>
2026-04-24 09:58:11 -0700

    Improve fallback street naming

--- server/src/utils/normalizeStreetName.js
+++ server/src/utils/normalizeStreetName.js
@@ -1,34 +1,75 @@
 const highwayTypeLabels = {
-  cycleway: 'Cycleway',
-  footway: 'Footpath',
-  living_street: 'Living street',
-  path: 'Path',
-  pedestrian: 'Pedestrian street',
-  residential: 'Residential street',
-  secondary: 'Secondary road',
-  secondary_link: 'Secondary link',
-  service: 'Service road',
-  tertiary: 'Tertiary road',
-  tertiary_link: 'Tertiary link',
-  track: 'Track',
-  trunk: 'Trunk road',
-  trunk_link: 'Trunk link',
-  unclassified: 'Local road',
+  cycleway: 'cycleway',
+  footway: 'footpath',
+  living_street: 'living street',
+  path: 'path',
+  pedestrian: 'pedestrian street',
+  residential: 'residential street',
+  secondary: 'secondary road',
+  secondary_link: 'secondary link',
+  service: 'service road',
+  tertiary: 'tertiary road',
+  tertiary_link: 'tertiary link',
+  track: 'track',
+  trunk: 'trunk road',
+  trunk_link: 'trunk link',
+  unclassified: 'local road',
+}
+
+function firstTagValue(tags, keys) {
+  for (const key of keys) {
+    const value = tags[key]
+
+    if (value) {
+      return value
+    }
+  }
+
+  return null
 }
 
 export function normalizeStreetName(tags = {}) {
-  if (tags.name) {
-    return tags.name
+  const namedValue = firstTagValue(tags, [
+    'name',
+    'official_name',
+    'alt_name',
+    'short_name',
+    'loc_name',
+  ])
+
+  if (namedValue) {
+    return namedValue
   }
 
-  if (tags.ref) {
-    return tags.ref
+  const routeValue = firstTagValue(tags, ['ref', 'nat_ref', 'int_ref', 'destination:ref'])
+
+  if (routeValue) {
+    return routeValue
+  }
+
+  if (tags.junction) {
+    return `${tags.junction} junction`
   }
 
   const highwayType = tags.highway
+  const surface = tags.surface
+  const access = tags.access
+  const service = tags.service
 
   if (highwayType && highwayTypeLabels[highwayType]) {
-    return `Unnamed ${highwayTypeLabels[highwayType].toLowerCase()}`
+    if (service) {
+      return `${highwayTypeLabels[highwayType]} (${service.replaceAll('_', ' ')})`
+    }
+
+    if (surface && highwayType === 'path') {
+      return `${surface.replaceAll('_', ' ')} path`
+    }
+
+    if (access === 'private') {
+      return `Private ${highwayTypeLabels[highwayType]}`
+    }
+
+    return `Unnamed ${highwayTypeLabels[highwayType]}`
   }
 
   return 'Unnamed local street'
